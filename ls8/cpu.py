2ddfb0c2f999e58597b0848e04406b34bdcb014b
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-05-19 08:23:33 -0700

    mvp day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,7 +11,7 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
 
         address = 0
@@ -20,13 +20,29 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            # 0b10000010, # LDI R0,8
+            # 0b00000000,
+            # 0b00001000,
+            # 0b01000111, # PRN R0
+            # 0b00000000,
+            # 0b00000001, # HLT
         ]
+
+        try:
+            with open(f"examples/{file}", "r") as file:
+                for line in file:
+                    comment = line.split("#")
+                    str_content = comment[0].strip()
+
+                    if str_content == "":
+                        continue
+                    # Convert binary string to integer
+                    byte = int(str_content, 2)
+
+                    program.append(byte)
+        except FileNotFoundError:
+            print("\033[1m" + f"{sys.argv[0]} {file} " + "\033[0m:" + " Not Found. Please make sure you are spelling it correctly.")
+            exit(1)
 
         for instruction in program:
             self.ram[address] = instruction
@@ -86,22 +102,29 @@ class CPU:
         LDI = 0b10000010 # 130 bytes
         # Print numeric value stored in the given register.
         PRN = 0b01000111 # 71 bytes
+        # Multiply the values in two registers together and store the result in registerA.
+        MUL = 0b10100010
 
         while not halted:
             # Instruction Register, contains a copy of the currently executing instruction
             IR = self.ram[self.pc]
             if IR == HLT:
-                print("Halting program")
                 halted = True
-                print("Halted")
+                print("🧮 Program Halted")
             elif IR == LDI:
-                address = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.register[address] = value
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+                self.register[operand_a] = operand_b
                 self.pc = self.pc + 3
+            elif IR == MUL:
+                number_one = self.ram_read(self.pc + 1)
+                number_two = self.ram_read(self.pc + 2)
+                self.register[number_one] = self.register[number_one] * self.register[number_two]
+                self.pc += 3
             elif IR == PRN:
-                address = self.ram_read(self.pc + 1)
-                print(self.register[address])
+                operand_a = self.ram_read(self.pc + 1)
+                print(self.register[operand_a])
                 self.pc = self.pc + 2
             else:
                 print(f"Cannot read instruction, {IR} at address {self.pc}")
+                halted = True
